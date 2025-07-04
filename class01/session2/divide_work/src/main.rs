edbd0e481a1bcf6360d97cdf9c84bef7d3654176
alchezar <kinder.wit@gmail.com>
2025-07-04 17:22:04 +0300

    Divide work

---
+++ class01/session2/divide_work/src/main.rs
@@ -0,0 +1,20 @@
+// IKinder
+
+fn main() {
+	const N_THREADS: usize = 8;
+	let to_add = (0..5000).collect::<Vec<u32>>();
+	let mut thread_handles = Vec::new();
+	let chunks = to_add.chunks(N_THREADS);
+	for chunk in chunks {
+		let my_chunk = chunk.to_owned();
+		let handle = std::thread::spawn(move || my_chunk.iter().sum::<u32>());
+		thread_handles.push(handle);
+	}
+
+	let sum = thread_handles
+		.into_iter()
+		.map(|h| h.join().unwrap())
+		.sum::<u32>();
+
+	println!("Total sum: {}", sum)
+}
