46ac9b0cca8c493ea51fdfc65d0fcd22b3c837be
alchezar <kinder.wit@gmail.com>
2025-07-07 17:49:10 +0300

    Hello async

--- class01/session3/src/theme/hello_async.rs
+++ class01/session3/src/theme/hello_async.rs
@@ -1,5 +1,41 @@
 ﻿// IKinder
 
+use futures::executor;
+use futures::future;
+
 pub fn main() {
 	crate::show_name(file!());
+
+	executor::block_on(say_hello());
+}
+
+async fn say_hello() {
+	println!("Hello, world!");
+	// second_function().await;
+	futures::join!(second_function(), say_goodbye());
+
+	let n = double(4).await;
+	println!("n: {}", n);
+
+	let futures = vec![double(1), double(2), double(3)];
+	let result = future::join_all(futures).await;
+	println!("result: {:?}", result);
+
+	do_something_sync();
+}
+
+async fn second_function() {
+	println!("Hello again");
+}
+
+async fn say_goodbye() {
+	println!("Goodbye");
+}
+
+async fn double(n: u32) -> u32 {
+	n * 2
+}
+
+fn do_something_sync() {
+	println!("Not async!");
 }
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@
 #![allow(dead_code, unused_variables, unused_imports, unused_mut)]
 #![warn(clippy::all, clippy::must_use_candidate)]
 
-use std::sync::mpsc;
-use std::sync::{Arc, Mutex};
-use std::thread;
-use std::time::Duration;
-
 fn main() {
 	println!("Hello, world!+");
 }
