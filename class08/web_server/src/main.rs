dafe253a5f094f2893b9f665fa2a4c910186e389
alchezar <kinder.wit@gmail.com>
2025-07-28 16:39:10 +0300

    Multi threaded server

--- class08/web_server/src/main.rs
+++ class08/web_server/src/main.rs
@@ -1,31 +1,41 @@
 // IKinder
 
-use std::io::{BufRead, Write};
+use std::fs::read_to_string;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread::sleep;
+use std::time::Duration;
+use web_server::ThreadPool;
 
 fn main() {
-	let listener = std::net::TcpListener::bind("localhost:7878").unwrap();
-	for stream in listener.incoming() {
+	let listener = TcpListener::bind("localhost:7878").unwrap();
+	let pool = ThreadPool::new(4);
+	for stream in listener.incoming().take(2) {
 		let stream = stream.unwrap();
-		println!("Connection established!");
-		handle_connection(stream);
+		pool.execute(|| {
+			handle_connection(stream);
+		})
 	}
 }
 
-fn handle_connection(mut stream: std::net::TcpStream) {
-	let buf_reader = std::io::BufReader::new(&mut stream);
+fn handle_connection(mut stream: TcpStream) {
+	let buf_reader = BufReader::new(&mut stream);
 	let request_line = buf_reader
 		.lines()
 		.next()
 		.unwrap()
 		.unwrap();
 
-	let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-		("HTTP/1.1 200 OK", "hello.html")
-	} else {
-		("HTTP/1.1 404 NOT FOUND", "404.html")
+	let (status_line, filename) = match &request_line[..] {
+		"GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+		"GET /sleep HTTP/1.1" => {
+			sleep(Duration::from_secs(5));
+			("HTTP/1.1 200 OK", "hello.html")
+		}
+		_ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
 	};
 
-	let contents = std::fs::read_to_string(format!("html/{filename}")).unwrap();
+	let contents = read_to_string(format!("html/{filename}")).unwrap();
 	let length = contents.len();
 
 	let response = format!("{status_line}\r\nContent-Type: text/html\r\nContent-Length: {length}\r\n\r\n{contents}");
