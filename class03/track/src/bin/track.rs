6350a66fd4a983e030b49c46c1cfafecae5b078c
alchezar <kinder.wit@gmail.com>
2025-08-30 16:30:25 +0300

    Testing setup

--- class03/track/src/bin/track.rs
+++ class03/track/src/bin/track.rs
@@ -2,6 +2,12 @@
 use track::error::AppError;
 use track::init;
 
+// track is the binary name
+//
+// track start
+// track stop
+// track report
+
 fn main() -> Result<(), AppError> {
 	init::error_reporting();
 	init::tracing();
