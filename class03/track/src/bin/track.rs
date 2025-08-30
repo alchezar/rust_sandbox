0d6c33f5dcc5c3901f61c8770b7ba5ae43283a74
alchezar <kinder.wit@gmail.com>
2025-08-30 20:31:13 +0300

    Cli parsing

--- class03/track/src/bin/track.rs
+++ class03/track/src/bin/track.rs
@@ -1,5 +1,6 @@
-﻿use error_stack::Result;
+﻿use error_stack::{Result, ResultExt};
 use track::error::AppError;
+use track::feature::cli;
 use track::init;
 
 // track is the binary name
@@ -11,6 +12,10 @@ use track::init;
 fn main() -> Result<(), AppError> {
 	init::error_reporting();
 	init::tracing();
+
+	cli::run()
+		.change_context(AppError)
+		.attach_printable("Failed to run CLI.")?;
 
 	Ok(())
 }
---
+++ class03/track/src/feature/cli.rs
@@ -0,0 +1,54 @@
+﻿use clap::Parser;
+
+#[derive(Debug, thiserror::Error)]
+#[error("CLI error occurred.")]
+pub struct CliError;
+
+#[derive(Debug, Clone, Copy, clap::Subcommand)]
+pub enum Command {
+	/// Start tracking time.
+	Start,
+	// Stop,
+	// Report,
+}
+
+#[derive(Debug, clap::Parser)]
+#[command(version, about, arg_required_else_help(true))]
+pub struct Cli {
+	#[command(subcommand)]
+	pub command: Command,
+}
+
+pub fn run() -> Result<(), CliError> {
+	// match command_builder().subcommand() {
+	// 	Some(("start", sub_matches)) => {
+	// 		// todo!()
+	// 	}
+	// 	_ => unreachable!(),
+	// }
+	let args = Cli::parse();
+	match args.command {
+		Command::Start => {
+			todo!();
+		}
+	}
+
+	// Ok(())
+}
+
+// fn command_builder() -> clap::ArgMatches {
+// 	clap::Command::new("track")
+// 		.subcommand(clap::Command::new("start").about("Start tracking time"))
+// 		.subcommand(clap::Command::new("stop").about("Stop tracking time"))
+// 		.subcommand(clap::Command::new("report").about("Generate a report"))
+// 		.arg(
+// 			clap::Arg::new("fluffy")
+// 				.short('f')
+// 				.long("fluffy")
+// 				.required(true)
+// 				.help("Good boy"),
+// 		)
+// 		.subcommand_required(true)
+// 		.arg_required_else_help(true)
+// 		.get_matches()
+// }
